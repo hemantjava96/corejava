2e89ecc90783baf491fc6f27d6748307a40841a3
hemantjava96 <hemantbesra96@gmail.com>
2024-12-02 22:31:59 +0530

    new changes

---
+++ src/com/hk/corejava/Data.java
@@ -0,0 +1,10 @@
+package com.hk.corejava;
+
+public class Data {
+    int data1;
+    int data2;
+    public void setData(int data1,int data2) {
+        this.data1 = data1;
+        this.data2 = data2;
+    }
+}
--- src/com/hk/corejava/Driver.java
+++ src/com/hk/corejava/Driver.java
@@ -1,11 +1,8 @@
 package com.hk.corejava;
 
-import java.util.HashMap;
-
 public class Driver {
     public static void main(String[] args) {
-        HashMap<String, String> map = new HashMap<>();
-        map.put("key1", "value1");
-        map.put("key2","value2");
+       Data data = new Data();
+       data.setData(4,5);
     }
 }
